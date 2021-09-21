7080b3b8360c506f6808b083d89dd8567ef7cd2b
Chris <orj.tsuk@gmail.com>
2021-09-21 08:17:30 -0400

    Getting testing set up and shipFactory working properly

--- src/ship.js
+++ src/ship.js
@@ -10,6 +10,19 @@ const shipFactory = (len) => {
       }
     }
   };
-  const isSunk = () => false;
+  const isSunk = () => {
+    let hitCount = 0;
+    for (let i = 0; i < shipArr.length; i += 1) {
+      if (shipArr[i] === "x") {
+        hitCount += 1;
+        if (hitCount === len) {
+          return true;
+        }
+      }
+    }
+    return false
+  };
   return { len, hit, isSunk, shipArr };
 };
+
+export default shipFactory;
