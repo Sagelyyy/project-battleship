2e1d6fd3130ea0201e5383da74b09100e8ff1acc
Chris <orj.tsuk@gmail.com>
2021-09-23 23:28:50 -0400

    More tweaks on gameboard and ship. Need to get the gameboard to recognize submarines..

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,5 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
 import shipFactory from "./ship.js";
 
 const gameboard = () => {
@@ -11,21 +13,6 @@ const gameboard = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ];
-  const clearBoard = () => {
-    for(let i = 0; i < playArea.length; i += 1){
-      playArea[i] = [0, 0, 0, 0, 0, 0, 0, 0]
-    }
-  }
-
-  // Gameboards should have a receiveAttack function 
-  // that takes a pair of coordinates, determines 
-  // whether or not the attack hit a ship and then 
-  // sends the ‘hit’ function to the correct ship, or 
-  // records the coordinates of the missed shot.
-
-  const receiveAttack = (x ,y) => {
-    // stuff
-  }
 
   const createShips = () => {
     const ships = {
@@ -33,29 +20,87 @@ const gameboard = () => {
       battleShip: shipFactory(4),
       cruiser: shipFactory(3),
       submarine: shipFactory(3),
-      destroyer: shipFactory(2)
-    }
-    return ships
-  }
+      destroyer: shipFactory(2),
+    };
+    return ships;
+  };
 
-  const ships = createShips()
+  const ships = createShips();
 
-  const placeShip = (x, y) => {
-    // ships.forEach(ship =>{ }) ??  maybe  ??
+  const clearBoard = () => {
     for (let i = 0; i < playArea.length; i += 1) {
-      let shipLen = ships.carrier.len;
-      while (shipLen > 0) {
-        playArea[(x + shipLen)-1][y] = 1;
-        shipLen -= 1;
+      playArea[i] = [0, 0, 0, 0, 0, 0, 0, 0];
+    }
+  };
+
+  // Gameboards should have a receiveAttack function
+  // that takes a pair of coordinates, determines
+  // whether or not the attack hit a ship and then
+  // sends the ‘hit’ function to the correct ship, or
+  // records the coordinates of the missed shot.
+
+  const receiveAttack = (x, y) => {
+    const shots = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    if (playArea[x][y] === 0) {
+      shots[x][y] = "m";
+    } else {
+      switch (playArea[x][y]) {
+        default:
+          console.log("something went wrong");
+          break;
+        case "c":
+          ships.carrier.hit();
+          break;
+        case "b":
+          ships.battleShip.hit();
+          break;
+        case "cr":
+          ships.cruiser.hit();
+          break;
+        case "s":
+          ships.submarine.hit();
+          break;
+        case "d":
+          console.log('d')
+          ships.destroyer.hit();
+          break;
       }
     }
   };
-  return { playArea, placeShip, clearBoard, ships};
-};
 
-const testGame = gameboard()
-testGame.placeShip(0, 5)
-console.log(testGame.playArea)
-console.log(testGame.ships)
+  const placeShip = (type, x, y) => {
+    let shipLen = type.len;
+    while (shipLen > 0) {
+      switch (type.len) {
+        default:
+          playArea[x + shipLen - 1][y] = "?";
+          break;
+        case 5:
+          playArea[x + shipLen - 1][y] = "c";
+          break;
+        case 4:
+          playArea[x + shipLen - 1][y] = "b";
+          break;
+        case 3:
+          playArea[x + shipLen - 1][y] = "cr";
+          break;
+        case 2:
+          playArea[x + shipLen - 1][y] = "d";
+          break;
+      }
+      shipLen -= 1;
+    }
+  };
+  return { playArea, placeShip, clearBoard, ships, receiveAttack };
+};
 
 export default gameboard;
--- src/ship.js
+++ src/ship.js
@@ -1,13 +1,8 @@
 const shipFactory = (len) => {
   const shipArr = [];
-  for (let i = 0; i < len; i += 1) {
-    shipArr.push(null);
-  }
-  const hit = (num) => {
-    for (let i = 0; i < shipArr.length; i += 1) {
-      if (shipArr[num] == null) {
-        shipArr[num] = "x";
-      }
+  const hit = () => {
+    if(shipArr.length < len){
+      shipArr.push('x')
     }
   };
   const isSunk = () => {
