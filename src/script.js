37c1d4fdd39e0b3f7285f06ed414b37edb50e0e2
Chris <orj.tsuk@gmail.com>
2021-10-10 21:31:00 -0400

    Still getting ui hooked up to scripts

--- src/script.js
+++ src/script.js
@@ -3,25 +3,27 @@ import gameboard from "./gameboard";
 import playerFactory from "./player";
 
 const MAIN = (function () {
-  const game = gameboard()
+  const game = gameboard();
   let player1;
   let player2;
 
   function boardSetup() {
-    let v = 8;
+    let v = 2;
     while (v > 0) {
-      for (let i = 0; i < 8; i += 1) {
+      for (let i = 0; i < 32; i += 1) {
         const sb = document.querySelector(".sb");
         const tile = document.createElement("div");
+        tile.classList.add('myTile')
         tile.style.width = "50px";
         tile.style.height = "50px";
         tile.style.backgroundColor = "lightblue";
         tile.style.border = "2px solid black";
         sb.appendChild(tile);
       }
-      for (let j = 0; j < 8; j += 1) {
+      for (let j = 0; j < 32; j += 1) {
         const gb = document.querySelector(".gb");
         const tile = document.createElement("div");
+        tile.classList.add('shotsTile')
         tile.style.width = "50px";
         tile.style.height = "50px";
         tile.style.backgroundColor = "lightgrey";
@@ -37,44 +39,66 @@ const MAIN = (function () {
     const desc = document.querySelector(".inputDesc");
     const player = playerFactory(input.value, "player");
     input.value = "";
-    desc.textContent = 'Enter player 2 name:'
+    desc.textContent = "Enter player 2 name:";
     return player;
   }
 
-  function getPlayers(){
-      console.log(player1)
-      console.log(player2)
+  function getPlayers() {
+    console.log(player1);
+    console.log(player2);
   }
 
-  function getShips(){
-    console.log(game.p1Ships)
-    console.log(game.p2Ships)
+  function getShips() {
+    console.log(game.p1Ships);
+    console.log(game.p2Ships);
   }
 
-  function gameSetup(){
-    //logic to place ships
+  function getBoards() {
+    console.log(game.boards.p1Board);
+    console.log(game.boards.p2Board);
   }
+
+  function renderShipPlacement(ship){
+    let len
+    switch(ship){
+      default:
+        console.log('something went wrong')
+        break;
+      case 'carrier':
+        len = 5
+        // hover over tile and display green for length of ship
+    }
+  }
+
+  function gameSetup() {
+    document.querySelector(".shipSelect").hidden = false;
+    document.querySelector(".carrierBtn").onclick = renderShipPlacement('carrier')
+  }
+
+
 
   const nameSubmit = document.querySelector(".nameSubmit");
   nameSubmit.onclick = function () {
-    if ((player1 === undefined)) {
+    if (player1 === undefined) {
       player1 = playerSetup();
     } else {
       player2 = playerSetup();
-      gameSetup()
+      gameSetup();
     }
   };
 
-  const btn = document.querySelector('.getPlayers')
-  btn.onclick = getPlayers
-  const shipsDev = document.querySelector('.getShips')
-  shipsDev.onclick = getShips
+  const btn = document.querySelector(".getPlayers");
+  btn.onclick = getPlayers;
+  const shipsDev = document.querySelector(".getShips");
+  shipsDev.onclick = getShips;
+  const boardsDev = document.querySelector(".getBoards");
+  boardsDev.onclick = getBoards;
 
   return {
     boardSetup,
     player1,
     player2,
-    getPlayers
+    getPlayers,
   };
 })();
 
