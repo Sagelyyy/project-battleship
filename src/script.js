7946be443c0966478617bfaf2b00d66453b407ad
Chris <orj.tsuk@gmail.com>
2021-10-11 22:08:28 -0400

    finally having the board fill in correctly when there is a ship placed!

--- src/script.js
+++ src/script.js
@@ -8,22 +8,24 @@ const MAIN = (function () {
   let player2;
 
   function boardSetup() {
-    let v = 2;
+    let v = 8;
     while (v > 0) {
-      for (let i = 0; i < 32; i += 1) {
+      for (let i = 0; i < 8; i += 1) {
         const sb = document.querySelector(".sb");
         const tile = document.createElement("div");
-        tile.classList.add('myTile')
+        tile.classList.add(`myTile${i}`);
+        tile.id = `tile${i}`;
+        tile.textContent = i;
         tile.style.width = "50px";
         tile.style.height = "50px";
         tile.style.backgroundColor = "lightblue";
         tile.style.border = "2px solid black";
         sb.appendChild(tile);
       }
-      for (let j = 0; j < 32; j += 1) {
+      for (let j = 0; j < 8; j += 1) {
         const gb = document.querySelector(".gb");
         const tile = document.createElement("div");
-        tile.classList.add('shotsTile')
+        tile.classList.add("shotsTile");
         tile.style.width = "50px";
         tile.style.height = "50px";
         tile.style.backgroundColor = "lightgrey";
@@ -58,24 +60,25 @@ const MAIN = (function () {
     console.log(game.boards.p2Board);
   }
 
-  function renderShipPlacement(ship){
-    let len
-    switch(ship){
-      default:
-        console.log('something went wrong')
-        break;
-      case 'carrier':
-        len = 5
-        // hover over tile and display green for length of ship
+  function boardRender(board) {
+    board = game.boards.p1Board
+    for (let row = 0; row < board.length; row += 1) {
+      for (let col = 0; col < board[row].length; col += 1) {
+        if (board[row][col] !== 0) {
+          const tiles = document.querySelectorAll(`.myTile${col}`);
+          for (let i = 0; i < board.length; i += 1) {
+            tiles[row].style.backgroundColor = "red";
+          }
+        }
+      }
     }
   }
 
   function gameSetup() {
     document.querySelector(".shipSelect").hidden = false;
-    document.querySelector(".carrierBtn").onclick = renderShipPlacement('carrier')
+    document.querySelector(".carrierBtn").onclick =
+      alert('ph')
   }
-
-
 
   const nameSubmit = document.querySelector(".nameSubmit");
   nameSubmit.onclick = function () {
@@ -93,6 +96,8 @@ const MAIN = (function () {
   shipsDev.onclick = getShips;
   const boardsDev = document.querySelector(".getBoards");
   boardsDev.onclick = getBoards;
+  const placeDev = document.querySelector('.placement')
+  placeDev.addEventListener('click', function(){boardRender(game.boards.p1Board)})
 
   return {
     boardSetup,
